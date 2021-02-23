0bcece907b9eb3f8242047ec660f2d1bbbf620fd
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-02-23 18:56:02 +0300

    add

--- js/fc.js
+++ js/fc.js
@@ -1,6 +1,15 @@
-var dollor = 1000.20
-var som = prompt("qancha somingiz bor")-0
-var y= som/dollor
-console.log(y)
+let dollar = 10000.20
+let ruble = 150
+if(confirm('Dollar almashtirmoqchimsiz')){
+   let sum = prompt("sum miqdorini kiriting" +"  " + "UZB->USD")
+   let natija = sum/dollar
+   alert("sizga beriladigan dollar miqdori->"+natija+"$"+"            "+"Bizbilan ishlaganiz uchun rahmat" +"         " +"Tastiqlash uchun OK tugmasini bosing")
+
+}else{
+    let sum = prompt("sum miqdorini kiriting"+ " "+"UZB->RUB")
+    let natija = sum/ruble
+    alert("sizga beriladigan ruble miqdori->"+natija+ "₽"+"               "+"Bizbilan ishlaganiz uchun rahmat" +"   " +"Tastiqlash uchun OK tugmasini bosing")
+ 
+}
 
 
