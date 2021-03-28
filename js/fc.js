287391d24c35ff144b060feee0aaf6771f202906
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-03-28 23:56:51 +0500

    add js

--- js/fc.js
+++ js/fc.js
@@ -1,32 +1,10 @@
+let listElement = document.querySelector(".list")
 
-let dollorni__sumga_nisbati = 9433.34
-let yevroni__sumga_nisbati = 10354.03
- alert("Sayohat uchun harajatlar")
- let ticek = prompt("borib kelish  yo'lkira  narhi faqat dollor qabul qiladi")-0
- let hotel = prompt("mehmon hona uchun  narhi faqat dollor qabul qiladi")-0
- let tirep = prompt("Kongil ochar maskanlar uchun yevro qabul qiladi")-0
- let alisherning__puli = prompt(" Alisher qancha puliz bor so'mda")-0
- let jamDollor = ticek+hotel
- let jamYevro = tirep
-
-
-function dollor(dollor){
-    return dollor/dollorni__sumga_nisbati
-}
-function yevro(yevro){
-    return yevro/yevroni__sumga_nisbati
-}
-let alisherning_dollor_puli = dollor(jamDollor)
-let alisherning__yevro_puli = yevro(jamYevro)
-
-let jamSuma = alisherning_dollor_puli + alisherning__yevro_puli
-   
-if(jamSuma>alisherning__puli){
-    console.log("Alisher siz biroz yana toplashiz kere ")
-}else{
-    console.log("Alisher yo'liz  behatar bo'lsin")
-}
-
+let a = <iframe width="560" height="315" src="https://www.youtube.com/embed/"
+let b = "title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+let itemElemnt = document.createElement("li");
+itemElemnt.innerHTML=a + b ;
+listElement.appendChild(itemElemnt);
 
 
 
